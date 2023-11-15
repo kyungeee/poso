36972866682d4e13863ad659091b9fc7df42bcd5
Wonder Park <pkkyung26@gmail.com>
2023-11-15 22:46:47 +0900

    Initial Commit

---
+++ poso/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  poso
+//
+//  Created by 박희경 on 2023/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ poso/posoApp.swift
@@ -0,0 +1,17 @@
+//
+//  posoApp.swift
+//  poso
+//
+//  Created by 박희경 on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct posoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
